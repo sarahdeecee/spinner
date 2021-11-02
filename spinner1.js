95dfc70ecba6db759c4a47453fb5148e8132f810
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2021-11-02 00:57:14 -0400

    spinner character spins in place once

---
+++ spinner1.js
@@ -0,0 +1,37 @@
+// process.stdout.write('hello from spinner1.js... \rheyyy\n');
+let time = 100;
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, time);
+time += 200;
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, time);
+time += 200;
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, time);
+time += 200;
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, time);
+time += 200;
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, time);
+time += 200;
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, time);
+time += 200;
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, time);
+time += 200;
+setTimeout(() => {
+  process.stdout.write('\r\\   '); 
+}, time);
+time += 200;
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, time);
